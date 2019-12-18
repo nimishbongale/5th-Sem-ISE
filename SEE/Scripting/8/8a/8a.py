8ce32d11699c4e4f94afecee92ac92718babb2f1
Nimish Bongale <nimishnb98@gmail.com>
2019-12-19 01:11:15 +0530

    Update 8a.py

--- SEE/Scripting/8/8a/8a.py
+++ SEE/Scripting/8/8a/8a.py
@@ -1,16 +1,16 @@
 def atomicdictionary():
     atomic = {"H":"Hydrogen","He":"Helium","Li":"Lithium","Be":"Berrylium","B":"Boron","C":"Carbon","N":"Nitrogen","F":"Fluorine","Ne":"Neon"}
     print(atomic)
-    sym = raw_input("Enter an existing symbol: ")
-    name = raw_input("Enter an element name: ")
+    sym = input("Enter an existing symbol: ")
+    name = input("Enter an element name: ")
     atomic[sym] = name
     print(atomic)
-    sym = raw_input("Enter a new symbol: ")
-    name = raw_input("Enter a new element name: ")
+    sym = input("Enter a new symbol: ")
+    name = input("Enter a new element name: ")
     atomic[sym] = name
     print(atomic)
     print("No. of elements: ",len(atomic))
-    ele=raw_input("Enter element to search: ")
+    ele=input("Enter element to search: ")
     # for i in atomic:
     #     if(i==ele):
     #         print("Element found!")
