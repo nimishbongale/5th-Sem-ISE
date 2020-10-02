1b95594be5f76ec723f244479701c9197b76b5dc
KSK-100 <72128007+KSK-100@users.noreply.github.com>
2020-10-02 18:58:56 +0530

    updated 1.sql

--- sem5DBMS/final/1.sql
+++ sem5DBMS/final/1.sql
@@ -1,41 +1,44 @@
 --sql code-- 
 
 
-create table e(ssn varchar(6),name varchar(10),deptno int, primary key(ssn));
+create table employee(ssn varchar(6),name varchar(10),deptno int, primary key(ssn));
 
-create table p (projectno varchar(10),projectarea varchar(20),primary key(projectno));
+create table project(projectno varchar(10),projectarea varchar(20),primary key(projectno));
 
-create table a(usn varchar(6),projectno varchar(10),foreign key(usn)references e(ssn),foreign key(projectno)references p(projectno));
-
-
-insert into e values('01','abc',10);
-insert into e values('02','xyz',20);
-insert into e values('03','pqr',30);
-insert into e values('04','lmn',40);
+create table assignedTo(usn varchar(6),projectno varchar(10),foreign key(usn)references employee(ssn),foreign key(projectno)references project(projectno));
 
 
-insert into p values('100','database');
-insert into p values('200','network');
-insert into p values('300','android');
-insert into a values('01','100');
+insert into employee values('01','abc',10);
+insert into employee values('02','xyz',20);
+insert into employee values('03','pqr',30);
+insert into employee values('04','lmn',40);
 
 
-insert into a values('02','200');
-insert into a values('03','300');
-insert into a values('01','200');
+insert into project values('100','database');
+insert into project values('200','network');
+insert into project values('300','android');
+                                                                                                                                               
+insert into assignedTo values('01','100');
+insert into assignedTo values('02','200');
+insert into assignedTo values('03','300');
+insert into assignedTo values('01','200');
 
-select * from e;
+--Displaying tables --                                                                                                                                    --
+select * from employee;
  
-select * from p;
+select * from project;
   
-select * from a;
+select * from assignedTo;
+
+--retrieving all employess who works on database project--
+select ssn from employee where ssn=(select usn from assignedTo where projectno=(select projectno from project where projectarea='database'));
+
+--count of all employees in each department--                                                                                
+select count(ssn),deptno from employee group by deptno;
   
-select ssn from e where ssn=(select usn from a where projectno=(select projectno from p where projectarea='database'));
-  
-select count(ssn),deptno from e group by deptno;
-  
-update a set projectno='200' where usn='03';
-select * from a;
+--updating project number whose usn is 3--                                                                             
+update assignedTo set projectno='200' where usn='03';                                                                                
+select * from aassignedTo;
   
   
   --mongodb code-- 
